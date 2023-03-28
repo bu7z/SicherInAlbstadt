3c1d844cc809625cb16cbe2fab4231c5d8b16716
bu7z <bu7z@bu7z-L.ad.fh-albsig.de>
2023-03-28 18:14:10 +0200

    finished login form

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-function showForm(){
+
+
+let toggle = 0;
+
+function showForm(e){
+    console.log("heloShowplz");
+    toggle = 1
+    if(e.id == "login"){
+        document.getElementById("logF").style.transform = "scale(1)";
+        document.getElementById("logF").style.transition = "1s";
+        return;
+    } else{
+
+    }
+    return;
+}
+
+
+function hideForm(e){
+    console.log("hideplz");
+    if (toggle != 0){
+        toggle = 0;
+        return;    
+    } else if(e.id == "logF"){
+        console.log("dontHidePlz");
+        return;
+    } else{
+        document.getElementById("logF").style.transform = "scale(0)";
+        document.getElementById("logF").style.transition = "1s";
+        console.log("test");
+    }
     
 }
