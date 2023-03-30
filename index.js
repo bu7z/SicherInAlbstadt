f3fbfc7a96cb59374b066cc2653d80b55c274ec7
bu7z <bu7z@bu7z-L.ad.fh-albsig.de>
2023-03-30 15:06:49 +0200

    finished reg

--- index.js
+++ index.js
@@ -9,10 +9,13 @@ function showForm(e){
         document.getElementById("logF").style.transform = "scale(1)";
         document.getElementById("logF").style.transition = "1s";
         return;
+    } else if(e.id == "reg"){
+        document.getElementById("regF").style.transform = "scale(1)";
+        document.getElementById("regF").style.transition = "1s";
+        return;
     } else{
-
+        return;
     }
-    return;
 }
 
 
@@ -21,11 +24,32 @@ function hideForm(e){
     if (toggle != 0){
         toggle = 0;
         return;    
-    } else if(e.id == "logF"){
+    } else if(e.id == "logF" || e.id == "regF"){
+        //dont hide
         return;
     } else{
         document.getElementById("logF").style.transform = "scale(0)";
         document.getElementById("logF").style.transition = "1s";
+        document.getElementById("regF").style.transform = "scale(0)";
+        document.getElementById("regF").style.transition = "1s";
     }
     
 }
+
+
+
+//password confirmation
+
+var password = document.getElementById("pwd")
+  , confirm_password = document.getElementById("pwdConf");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
