f1d6dd35a0b2e9aac78373dd3bb1887da8af943b
Benjamin Mako <gordon.shumway1024@gmail.com>
2023-04-13 18:06:38 +0200

    Replace all whitespaces with tabs. Error message still buggy.

--- chat_page.js
+++ chat_page.js
@@ -1,3 +1,26 @@
+// need to beautify this shit !!!
+
+let element_chat_window;
+let element_input_field;
+let element_error_message;
+
+document.addEventListener("DOMContentLoaded", () => {
+  element_chat_window = document.getElementById("chat_main");
+  element_input_field = document.getElementById("chat_input_field");
+  element_error_message = document.getElementById("div_error_message");
+
+  element_error_message.style.display = "none";
+  document.getElementById("header_chat_text").style.display = "none";
+
+  console.log("DOM ready");
+});
+
+// scroll down chat window to bottom
+function autoscroll() {
+  element_chat_window.scrollTop = element_chat_window.scrollHeight;
+}
+
+// rename function; nav change on hover
 function toggle_visibility(element) {
   if (element.children[0].style.display == "none") {
     element.children[0].style.display = "block";
@@ -9,27 +32,36 @@ function toggle_visibility(element) {
   }
 }
 
-// need to beautify this shit
-function click_button(element) {
-  // create inner and outer div and add to chat window
-  const div_1 = document.createElement("div");
-  const div_2 = document.createElement("div");
-  const chat_window = document.getElementById("chat_main");
-  const input_field = document.getElementById("chat_input_field");
-  const node = document.createTextNode(input_field.value);
-
-  div_2.appendChild(node);
-  div_1.appendChild(div_2);
-
-  div_1.classList.add("chat_bubble");
-  div_1.classList.add("chat_bubble_sent");
-
-  chat_window.appendChild(div_1);
-
-  // make sure it automatically scrolls down to latest message when send
-  chat_window.scrollTop = chat_window.scrollHeight;
-
-  input_field.value = "";
+function set_error_message() {
+  element_error_message.style.display = "block";
+  autoscroll();
 }
 
-document.getElementById("header_chat_text").style.display = "none";
+function reset_error_message() {
+  element_error_message.style.display = "none";
+}
+
+function click_button(element) {
+  // create inner and outer div and add to chat window
+  const element_div_outer = document.createElement("div");
+  const element_div_inner = document.createElement("div");
+  const message = element_input_field.value;
+  const node = document.createTextNode(message);
+
+  if (message == "") {
+    set_error_message();
+  } else {
+    element_div_inner.appendChild(node);
+    element_div_outer.appendChild(element_div_inner);
+
+    element_div_outer.classList.add("chat_bubble");
+    element_div_outer.classList.add("chat_bubble_sent");
+
+    element_chat_window.appendChild(element_div_outer);
+
+    // make sure it automatically scrolls down to latest message when send
+    autoscroll();
+
+    element_input_field.value = "";
+  }
+}
