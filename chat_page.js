5b4f8624dc6239b9dfff41cf645d03a6d3dcfbdc
Benjamin Mako <gordon.shumway1024@gmail.com>
2023-04-13 09:34:16 +0200

    save this shit cause i'm not able to reproduce

--- chat_page.js
+++ chat_page.js
@@ -9,9 +9,27 @@ function toggle_visibility(element) {
   }
 }
 
+// need to beautify this shit
 function click_button(element) {
-  const text = element.value;
-  console.log(text);
+  // create inner and outer div and add to chat window
+  const div_1 = document.createElement("div");
+  const div_2 = document.createElement("div");
+  const chat_window = document.getElementById("chat_main");
+  const input_field = document.getElementById("chat_input_field");
+  const node = document.createTextNode(input_field.value);
+
+  div_2.appendChild(node);
+  div_1.appendChild(div_2);
+
+  div_1.classList.add("chat_bubble");
+  div_1.classList.add("chat_bubble_sent");
+
+  chat_window.appendChild(div_1);
+
+  // make sure it automatically scrolls down to latest message when send
+  chat_window.scrollTop = chat_window.scrollHeight;
+
+  input_field.value = "";
 }
 
 document.getElementById("header_chat_text").style.display = "none";
